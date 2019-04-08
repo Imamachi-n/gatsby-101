d6f312241073f6b9c58be40b039ede987e5021e2
imamachi-n <imamachigatugatu@hotmail.co.jp>
2019-04-08 21:43:11 +0900

    learn getting relative markdown filepath using Gatsby API

---
+++ tutorial-part-four/gatsby-node.js
@@ -0,0 +1,9 @@
+exports.onCreateNode = ({
+  node,
+  getNode
+}) => {
+  if (node.internal.type === `MarkdownRemark`) {
+    const fileNode = getNode(node.parent)
+    console.log(`\n`, fileNode.relativePath)
+  }
+}
