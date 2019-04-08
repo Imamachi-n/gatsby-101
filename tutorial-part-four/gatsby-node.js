11c0c4657ea4533eed5ee87ba8952134d0de88ed
imamachi-n <imamachigatugatu@hotmail.co.jp>
2019-04-09 07:41:35 +0900

    learn how to build page template components

--- tutorial-part-four/gatsby-node.js
+++ tutorial-part-four/gatsby-node.js
@@ -1,3 +1,4 @@
+const path = require(`path`)
 const {
   createFilePath
 } = require(`gatsby-source-filesystem`)
@@ -28,6 +29,10 @@ exports.createPages = ({
   graphql,
   actions
 }) => {
+  const {
+    createPage
+  } = actions
+
   // **Note:** The graphql function call returns a Promise
   // see: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise for more info
   return graphql(`
@@ -43,6 +48,18 @@ exports.createPages = ({
       }
     }
   `).then(result => {
-    console.log(JSON.stringify(result, null, 4))
+    result.data.allMarkdownRemark.edges.forEach(({
+      node
+    }) => {
+      createPage({
+        path: node.fields.slug,
+        component: path.resolve(`./src/templates/blog-post.js`),
+        context: {
+          // Data passed to context is available
+          // in page queries as GraphQL variables.
+          slug: node.fields.slug,
+        },
+      })
+    })
   })
 }
